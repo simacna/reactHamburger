2e06b271a398a9d5a3e41999a774e8224cc2ef96
sina sima <simacna@gmail.com>
2020-02-27 15:37:23 -0500

    creating error comp

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -1,1 +1,12 @@
 import React, {Component} from 'react';
+
+class ErrorBoundary extends Component {
+    state = {
+        hasError: false,
+        errorMessage: ''
+    }
+}
+
+componentDidCatch = (error, info) => {
+    this.ListeningStateChangedEvent({hasError: true, errorMessage: error});
+}
