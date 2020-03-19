f61adfcc876153b2748bf841bbdd64a1a981b9e8
sina sima <simacna@gmail.com>
2020-03-18 20:53:55 -0400

    adding another notes

---
+++ public/notes.js
@@ -0,0 +1,10 @@
+4.18.20
+
+// class based 
+// class X extends Components 
+// histortorically, accessing state, they were class based
+//acess Access to State, Lifecycle hooks, access to state and props via "this" (this.state.X & this.props.X)
+
+
+// v functional components
+// access to state (useState()), no access to lifecycle hooks, access props via 'props' (props.X)
