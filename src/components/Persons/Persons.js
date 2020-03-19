1c79dbf3eef9c232aa2ab966606f8ab6e60e21ae
sina sima <simacna@gmail.com>
2020-03-18 20:26:21 -0400

    hm personal laptop

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,9 +4,13 @@ import React from 'react';
 import Person from './Person/Person';
 
 
-const persons = (props) => props.persons.map((person, index) => {
+const persons = (props) => {
+    console.log('Persons.js - rendering...');
+
+    return props.persons.map((person, index) => {
 //   below returning a list - q: how do you know <Person> is a list?
-    return <Person  
+    return (
+        <Person  
             click={() => props.clicked(index)}
             name={person.name}
             age={person.age}
@@ -14,7 +18,9 @@ const persons = (props) => props.persons.map((person, index) => {
             changed={(event) => props.changed(event, person.id)}
 
     />
-});
+        );
+    });
+};
 
 export default persons;
 
